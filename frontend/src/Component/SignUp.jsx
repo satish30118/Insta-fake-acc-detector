4954bd49538605c8c0ba6815d947d13eef01f920
Aditya <amankumar199j@gmail.com>
2024-04-11 16:26:28 +0530

    style

--- frontend/src/Component/SignUp.jsx
+++ frontend/src/Component/SignUp.jsx
@@ -39,7 +39,7 @@ const SignUp = () => {
     }
 
     try {
-     
+
       const res = await axios.post(
         `${process.env.REACT_APP_API}/api/v1/auth/register`,
         { name, email, password }
@@ -62,134 +62,91 @@ const SignUp = () => {
 
   return (
     <>
-      <div className="signup-form">
-        <section
-          className="vh-100"
-          style={{ backgroundColor: "rgb(118, 207, 207)" }}
-        >
-          <div className="container h-100">
-            <div className="row d-flex justify-content-center align-items-center h-100">
-              <div className="col-lg-12 col-xl-11">
-                <div
-                  className="card text-black"
-                  style={{ borderRadius: "25px" }}
-                >
-                  <div className="card-body p-md-5 mb-1">
-                    <div className="row justify-content-center">
-                      <div className="col-md-10 col-lg-6 col-xl-5 order-2 order-lg-1">
+      <div className="signin-page">
 
-                        <p className="text-center h1 fw-bold mb-1 mx-1 mx-md-1 mt-1">
-
-                          Sign up
-                        </p>
-
-                        <form className="mx-1 mx-md-1">
-                          <div className="d-flex flex-row align-items-center mb-4">
-                            <i className="fas fa-user fa-lg me-3 fa-fw"></i>
-                            <div
-                              data-mdb-input-init
-                              className="form-outline flex-fill mb-0"
-                            >
-                              <input type="text" placeholder="Enter Name" name="name" onChange={handleData}/>
-                            </div>
-                          </div>
-
-                          <div className="d-flex flex-row align-items-center mb-4">
-                            <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
-                            <div
-                              data-mdb-input-init
-                              className="form-outline flex-fill mb-0"
-                            >
-                              <input type="email" placeholder="Enter email" name="email" onChange={handleData}/>
-                            </div>
-                          </div>
-
-                          <div className="d-flex flex-row align-items-center mb-4">
-                            <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
-                            <div
-                              data-mdb-input-init
-                              className="form-outline flex-fill mb-0"
-                            >
-                              <input
-                                type="password"
-                                placeholder="Enter Password"
-                                name="password"
-                                onChange={handleData}
-                              />
-                            </div>
-                          </div>
-
-                          <div className="d-flex flex-row align-items-center mb-4">
-                            <i className="fas fa-key fa-lg me-3 fa-fw"></i>
-                            <div
-                              data-mdb-input-init
-                              className="form-outline flex-fill mb-0"
-                            >
-                              <input
-                                type="password"
-                                placeholder="Re-enter Password"
-                                name="cpassword"
-                                onChange={handleData}
-                              />
-                            </div>
-                          </div>
-
-                          <div className="form-check d-flex justify-content-center mb-5">
-                            
-                            <label
-                              className="form-check-label"
-                              for="form2Example3"
-                            >
-                              Already have an Account?
-                              <Link to="/sign-in">Login</Link>
-                            </label>
-                            <label
-                              className="form-check-label"
-                              for="form2Example3"
-                            >
-                              Predict Insta fake Account?
-                              <Link to="/predict/insta-fake-account">
-                                Insta
-                              </Link>
-                            </label>
-                            <label
-                              className="form-check-label"
-                              for="form2Example3"
-                            >
-                              Predict tweet sentiment?
-                              <Link to="/predict/tweet-status">Tweet</Link>
-                            </label>
-                          </div>
-
-                          <div className="d-flex justify-content-center mx-4 mb-3 mb-lg-4">
-                            <button
-                              type="button"
-                              data-mdb-button-init
-                              data-mdb-ripple-init
-                              className="btn btn-primary btn-lg"
-                              onClick={sendData}
-                            >
-                              Register
-                            </button>
-                          </div>
-                        </form>
-                      </div>
-                      <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
-                        <img
-                          src={
-                            "https://img.freepik.com/premium-vector/young-woman-cartoon_18591-42804.jpg"
-                          }
-                          className="img-fluid"
-                          alt="Sample image"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                </div>
+        <form >
+          <div className="signin-form">
+          <h1 className="signin-head">Sign Up</h1>
+            <div className="d-flex flex-row align-items-center mb-4">
+              <i className="fas fa-user fa-lg me-3 fa-fw"></i>
+              <div
+                data-mdb-input-init
+                className="form-outline flex-fill mb-0"
+              >
+                <input type="text" placeholder="Enter Name" name="name" onChange={handleData} />
               </div>
             </div>
+
+            <div className="d-flex flex-row align-items-center mb-4">
+              <i className="fas fa-envelope fa-lg me-3 fa-fw"></i>
+              <div
+                data-mdb-input-init
+                className="form-outline flex-fill mb-0"
+              >
+                <input type="email" placeholder="Enter email" name="email" onChange={handleData} />
+              </div>
+            </div>
+
+            <div className="d-flex flex-row align-items-center mb-4">
+              <i className="fas fa-lock fa-lg me-3 fa-fw"></i>
+              <div
+                data-mdb-input-init
+                className="form-outline flex-fill mb-0"
+              >
+                <input
+                  type="password"
+                  placeholder="Enter Password"
+                  name="password"
+                  onChange={handleData}
+                />
+              </div>
+            </div>
+
+            <div className="d-flex flex-row align-items-center mb-4">
+              <i className="fas fa-key fa-lg me-3 fa-fw"></i>
+              <div
+                data-mdb-input-init
+                className="form-outline flex-fill mb-0"
+              >
+                <input
+                  type="password"
+                  placeholder="Re-enter Password"
+                  name="cpassword"
+                  onChange={handleData}
+                />
+              </div>
+            </div>
+
+            <div className="form-check d-flex justify-content-center mb-5">
+
+              <label
+                className="form-check-label"
+                for="form2Example3"
+              >
+                Already have an Account?
+                <Link to="/sign-in">Login</Link>
+              </label>
+            </div>
+            <div >
+              <button
+                type="button"
+                className="m-btn"
+                onClick={sendData}
+              >
+                Register
+              </button>
+            </div>
           </div>
-        </section>
+          <div className="col-md-10 col-lg-6 col-xl-7 d-flex align-items-center order-1 order-lg-2">
+            <img
+              src={
+                "https://img.freepik.com/premium-vector/young-woman-cartoon_18591-42804.jpg"
+              }
+              className="img-fluid"
+              alt="Sample image"
+            />
+          </div>
+        </form>
       </div>
     </>
   );
